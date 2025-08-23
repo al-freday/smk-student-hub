3e49c4b584b4b1840a219995ac79ae5c5555b83b
Alfreday Pare Layuk <alfredayparelayuk@gmail.com>
2025-08-23 14:20:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/manajemen-siswa/page.tsx
+++ src/app/dashboard/manajemen-siswa/page.tsx
@@ -42,6 +42,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { useToast } from "@/hooks/use-toast";
 import { format } from "date-fns";
